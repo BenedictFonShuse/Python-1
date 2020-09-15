1471a8deb57c571c05c913f476f6417e88eb7592
Benedict <fb358@mail.ru>
2020-09-15 12:40:19 +0300

    Добавлена проверка номера месяца

--- les-2/task3.py
+++ les-2/task3.py
@@ -10,11 +10,15 @@ list_of_month = [1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 1]
 
 
 while True:
-    print("Введите число")
+    print("Введите номер месяца от 1 до 12")
     input_int = input()
     if input_int.isdigit():
         input_int = int(input_int)
-        break
-    print("Введено отличное от числа значение")
+        if 0 < input_int <= 12:
+            break
+        else:
+            print("Введено отличное от требуемого число")
+    else:
+        print("Введено отличное от числа значение")
 
 print(seasons[list_of_month[input_int]])
